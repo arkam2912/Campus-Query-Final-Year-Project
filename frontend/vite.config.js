f280c9fd2b48cda4439ea8fe47311f7c49b30d87
Kashif Ahmad <129614929+kashifgour@users.noreply.github.com>
2025-04-27 21:35:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+await axios.post('https://campus-query-final-year-project.onrender.com/ask', { query: 'hello' })
